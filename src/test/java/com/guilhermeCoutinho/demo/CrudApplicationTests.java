cd4033bf1e0c63cd7c6df617440dbdc050b16033
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2025-07-21 21:08:02 -0300

    adicionando projeto

---
+++ src/test/java/com/guilhermeCoutinho/demo/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeCoutinho.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
